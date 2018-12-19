866e7db2634c5d628b737146784f6e43c0518020
Tameesh Biswas <tameeshbiswas1998@gmail.com>
2018-12-19 10:26:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ const  express = require('express');
 const body_parser = require('body-parser');
 const logger = require('morgan');
 const listenPort = process.env.PORT  || 3000 ;
+var ps = require('python-shell');
 
 let app = express();
 app.use(body_parser.urlencoded({extended: false}));
 app.use(body_parser.json());
 
-app.post("/", (req, res)=> {
-    console.log(req.body);
-    res.send(JSON.stringify(req.body.queryResult.parameters));
+app.get("/", (req, res)=> {
+    let options = {
+        mode: 'text',
+        pythonPath: '/usr/local/bin/python3',
+        args: ['tv']
+      };
+    ps.PythonShell.run('fl.py', options, function (err, results) { 
+        console.log(results)
+    });
+    // console.log(req.body);
+    // res.send(JSON.stringify(req.body.queryResult.parameters));
+    res.send("a");
 }) 
 
 //server setup
