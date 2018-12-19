7946fcf4fe2dfc57074dce84c803866b56c0e700
ivary43 <contactmanish16@gmail.com>
2018-12-19 09:14:18 +0530

    step 2

---
+++ app.js
@@ -0,0 +1,17 @@
+const  express = require('express');
+const body_parser = require('body-parser');
+const logger = require('morgan');
+const listenPort = process.env.PORT  || 3000 ;
+
+let app = express();
+app.use(body_parser.urlencoded({extended: false}));
+app.use(body_parser.json());
+
+app.post("/", (req, res)=> {
+    console.log(req.body);
+}) 
+
+//server setup
+app.listen(listenPort, ()=>{
+    console.log('The server is running at :', listenPort);
+});
